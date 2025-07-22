b52c80231fcd0a0f4e3b5a3d0a868a34479534f1
Hxrob <tun85603@temple.edu>
2025-07-22 03:27:15 -0400

    added Front Page photos, and Amenities section

--- one-point-five/src/app/page.tsx
+++ one-point-five/src/app/page.tsx
@@ -3,8 +3,81 @@ import Image from 'next/image';
 
 export default function HomePage() {
   return (
-    <div>
-         <h1 className="text-4xl p-8">Welcome to the Home Page</h1>
-    </div>
+    <>
+      <div className="relative w-full h-screen">
+        <Image src="/images/frontpage.jpg" alt="One Point Five Front Entrance" fill className="object-cover"/>
+        <div className="absolute inset-0 flex items-center justify-center z-10">
+          <Image src="/images/headerlogo.png" alt="One Point Five Logo" width={800} height={200} className="opacity-90 brightness-0 invert"/>
+        </div>
+      </div>
+
+
+      {/* Amenities Section */}
+      <section className="py-24 mt-16">
+        <div className="container mx-auto px-4 flex flex-col w-full h-[429px]">
+          <div className="text-center justify-center text-slate-800 text-5xl font-normal font-['Kaisei_Tokumin'] mb-30">
+            Amenities
+          </div>
+
+          <div className="flex justify-center items-start space-x-16">
+
+            {/* Wifi */}
+            <div className="flex flex-col items-center text-center w-32">
+              <div className="relative w-24 h-24 mb-4">
+                <Image src="/images/wifi.svg" alt="Wifi" fill className="object-contain"/>
+              </div>
+              <div className="w-36 text-center justify-center text-slate-800 text-2xl font-normal font-['Kaisei_Tokumin'] leading-10">
+                Free Wi-Fi
+              </div>
+            </div>
+
+            {/* Kitchen */}
+            <div className="flex flex-col items-center text-center w-32">
+              <div className="relative w-24 h-24 mb-4">
+                <Image src="/images/kitchen.svg" alt="Kitchen" fill className="object-contain"/>
+              </div>
+              <div className="w-36 text-center justify-center text-slate-800 text-2xl font-normal font-['Kaisei_Tokumin'] leading-10">
+                 Kitchen Space
+              </div>
+            </div>
+
+            {/* Laundry */}
+            <div className="flex flex-col items-center text-center w-32">
+              <div className="relative w-24 h-24 mb-4">
+                <Image src="/images/laundry.svg" alt="Laundry" fill className="object-contain"/>
+              </div>
+              <div className="w-36 text-center justify-center text-slate-800 text-2xl font-normal font-['Kaisei_Tokumin'] leading-10">
+                Washing Machine
+              </div>
+            </div>
+
+            {/* Shower & Bathtub */}
+            <div className="flex flex-col items-center text-center w-32">
+              <div className="relative w-24 h-24 mb-4">
+                <Image src="/images/bathtub.svg" alt="Bathtub" fill className="object-contain"/>
+              </div>
+              <div className="w-36 text-center justify-center text-slate-800 text-2xl font-normal font-['Kaisei_Tokumin'] leading-10">
+                Shower & Bathtub
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="w-full h-[3326px] bg-stone-50">
+
+          <div className="justify-center text-slate-800 text-5xl text-center font-normal font-['Kaisei_Tokumin']">
+            Rooms
+          </div>
+
+
+          
+
+
+        </div>
+      </section>
+    </> 
+
   );
 }
--- one-point-five/src/components/Header.tsx
+++ one-point-five/src/components/Header.tsx
@@ -3,24 +3,24 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="sticky top-0 bg-yellow-50 w-full h-[60px] border-b">
+    <header className="sticky top-0 bg-[#FDF9EF] w-full h-[75px] z-50">
       <div className="max-w-full mx-auto h-full flex items-center justify-between px-4">
         {/* Logo */}
-        <div className="h-[50px]">
-          <Image src="/images/headerlogo.png" alt="One Point Five" width={200} height={50} />
+        <div className="h-[75px] ml-8">
+          <Image src="/images/headerlogo.png" alt="One Point Five" width={300} height={75} />
         </div>
         {/* Navigation */}
-        <nav className="flex items-center space-x-8">
+        <nav className="flex items-center space-x-20">
           <Link href="/" className="flex items-center font-medium font-jp text-gray-800 text-[20px]">
               Home <span className="ml-1">▾</span>
           </Link>
-          <Link href="/rooms" className="font-medium text-gray-800 text-[20px]">
+          <Link href="/rooms" className="font-medium text-[#1C2541] text-[20px]">
             Rooms
           </Link>
-          <Link href="/locations" className="font-medium text-gray-800 text-[20px]">
+          <Link href="/locations" className="font-medium text-[#1C2541] text-[20px]">
             Nearby
           </Link>
-          <Link href="/contact" className="font-medium text-gray-800 text-[20px]">
+          <Link href="/contact" className="font-medium text-[#1C2541] text-[20px]">
             Book Now
           </Link>
         </nav>
